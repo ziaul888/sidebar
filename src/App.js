c509420329d658c65da2eceb6a1b1a93fd92d7e5
ziaul888 <ziaul935@gmail.com>
2021-03-29 00:58:06 +0600

    hook

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const[name,setName]=useState("default")
+  const [income,setIncome]=useState("high");
+ 
+  const handlechange=(e)=>{
+    setName(e.target.value)
+  }
+
+  const handleIncomechange=(e)=>{
+    setIncome(e.target.value)
+  }
+ const handleSubmit=()=>{
+   console.log("state=",name,income);
+ }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <span>name:</span>
+              <input type="text" value={name} onChange={handlechange}></input>
+              <span>income</span>
+              <select onChange={handleIncomechange}>
+                <option value="high">income:high</option>
+                <option value="mid">income:mid</option>
+                <option value="low">income:low</option>
+              </select>
+              <input type="submit" value="submit"></input>
+          </div>
+        </form>
       </header>
     </div>
   );
